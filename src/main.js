ce90352ea4af2a9a89b1d6153142a6c6819bb377
Oghenekaro Emmanuella <oghenekaro57@gmail.com>
2021-09-06 18:10:09 +0100

    input box component

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
+import input from './components/inputBox.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+Vue.component('input-box', input)
 
 new Vue({
   router,
