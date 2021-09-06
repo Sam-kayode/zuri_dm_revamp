84cd13e8f7fa717202c330125362630284556096
abisalde <abisalde>
2021-09-06 05:00:51 -0400

    create-schedule message

---
+++ src/components/scheduleMessageModal.vue
@@ -0,0 +1,38 @@
+<template> 
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container mb-3" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
+                        <header class="modal-header" id="modalTitle">
+                        <slot name="header">
+                            Schedule message
+                        </slot>
+                        </header>
+                        <section class="modal-body" id="modalDescription">
+                            <slot name="body">
+                                <p>Later today at 9:00 AM</p>
+                                <p>Monday at 9:00 AM</p>
+                                <hr>
+                                <p>Custom time</p>
+                        </slot>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+
+<script>
+
+
+export default {
+    name: 'ScheduleMessageModal',
+    methods: {
+        close() {
+            this.$emit('close');
+        },
+    },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Team Orpheus"/>
+    <b-button class="button" variant="success">
+        <img alt="button-send" src="../assets/Shape-send.png" />
+        <b-icon icon="chevron-down" aria-hidden="true"></b-icon>
+    </b-button>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+// import ScheduleMessage from '@/components/ScheduleMessage.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
