7f19da0c9d3897e4fdbb9c3382712d756344d08d
utitofon <69417647+Utitofon-Udoekong@users.noreply.github.com>
2021-09-06 16:01:40 +0100

    Revert "Added reply to thread component"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="home d-flex flex-column">
-    <ThreadReply />
+    
     <ThreadCalender />
 
     <b-button class="button" variant="success" size="sm" pb="2">
         <img alt="button-send" src="../assets/Shape-send.png" />
         <b-icon icon="chevron-down" aria-hidden="true"></b-icon>
-    </b-button>
-    <b-button class="button" v-b-toggle.sidebar variant="success" size="sm" pb="2">
-        <b-icon icon="reply" aria-hidden="true"></b-icon>
     </b-button>
     <ScheduleMessageModal />
   </div>
@@ -18,14 +15,12 @@
 // @ is an alias to /src
 import ThreadCalender from '@/components/threadCalender.vue'
 import ScheduleMessageModal from '@/components/scheduleMessageModal.vue'
-import ThreadReply from '@/components/threadReply.vue'
 
 export default {
   name: 'Home',
   components: {
     ThreadCalender,
-    ScheduleMessageModal,
-    ThreadReply
+    ScheduleMessageModal
   }
 }
 </script>
