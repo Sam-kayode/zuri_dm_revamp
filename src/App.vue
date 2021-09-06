54baf8ddbd5d029d4d7bb6ca37c6ab0d970f6c65
Jesutooni <edeworuyoyou@gmail.com>
2021-09-06 18:08:25 +0100

    added search bar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <searchbar></searchbar>
   </div>
 </template>
+
+<script>
+import searchbar from './components/searchbar.vue'
+export default {
+  name: 'app',
+  components: {
+    searchbar
+  }
+}
+</script>
 
 <style>
 #app {
