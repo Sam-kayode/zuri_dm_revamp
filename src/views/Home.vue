735aa099c32fb9fbee929c6b36a3581013109586
Utitofon-Udoekong <mosesrahul37@gmail.com>
2021-09-06 15:39:00 +0100

    created reply thread section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,24 @@
   <div class="home d-flex justify-content-center">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Team Orpheus"/>
-    <thread_calender/>
+    <b-container>
+      <thread_calender/>
+      <b-button v-b-toggle.sidebar>Show sidebar</b-button>
+    </b-container>
+    <thread-reply></thread-reply>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import thread_calender from '@/components/threadCalender.vue'
+import ThreadReply from '@/components/threadReply.vue'
 
 export default {
   name: 'Home',
   components: {
-    thread_calender
+    thread_calender,
+    ThreadReply
   }
 }
 </script>
