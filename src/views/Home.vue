0a4e420e4bbe7de05ef5904b2f245448427a4dbd
unknown <demilad1998@gmaill.com>
2021-09-06 18:25:05 +0100

    thread action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="home d-flex justify-content-center">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Team Orpheus"/>
+   
     <thread_calender/>
+    <thread-actions></thread-actions>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import thread_calender from '@/components/threadCalender.vue'
+import threadActions from '../components/dmThreadActions.vue' 
 
 export default {
   name: 'Home',
   components: {
-    thread_calender
+    thread_calender,
+    threadActions
   }
 }
 </script>
