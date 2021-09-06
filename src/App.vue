7716431b520a9a41504bddf29bb58a0f2107c45c
abisalde <abisalde>
2021-09-06 11:59:47 -0400

    reviews

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <!-- <div id="nav">
+    <div id="app">
+        <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-    <router-view/>
-  </div>
+        <router-view />
+    </div>
 </template>
-
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
-*, *:before, *:after {
-  box-sizing: border-box;
-  padding: 0;
-  margin: 0;
-  font-family: 'Lato', sans-serif;
+*,
+*:before,
+*:after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: 'Lato', sans-serif;
 }
-
 
 li {
-  list-style: none;
+    list-style: none;
 }
 
-
-
+a {
+    text-decoration: none;
+}
 </style>
