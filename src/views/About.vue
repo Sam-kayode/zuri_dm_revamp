4c8a53bcd8bc3fd0b08c8ec98d4366c81a713f7f
Ayomide26 <ayomidefadare26@gmail.com>
2021-09-06 17:34:55 +0100

    Created new component and implemented messageInput in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
+
+
 <template>
-  <div class="about">
+<main>
+  <Messageinput />
+    <div class="about">
     <h1>This is an about page</h1>
   </div>
+</main>
 </template>
+
+<script>
+
+import Messageinput from '../components/Messageinput';
+
+export default {
+  components: {
+    Messageinput
+  }
+}
+</script>
+
+<style>
+
+</style>
