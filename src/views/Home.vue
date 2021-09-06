1316834bd786ac17c5f51f267e4bd2b80db86c38
moabah <moabadegbite@gmail.com>
2021-09-06 20:37:41 +0100

    created profile header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProfileHeader/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import ProfileHeader from '@/components/ProfileHeader.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    ProfileHeader
   }
 }
 </script>
