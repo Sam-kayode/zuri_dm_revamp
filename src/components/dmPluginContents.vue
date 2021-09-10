e15232655c8a7f71d3bbf653281d17759ddbc508
abisalde <abisalde>
2021-09-10 05:58:32 -0400

    input-box

--- src/components/dmPluginContents.vue
+++ src/components/dmPluginContents.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="all-dm-messages py-3 px-4">
+    <div class="all-dm-messages py-3 px-2 position-relative">
         <ThreadCalendar />
         <DmMessageThread />
+        <InputField />
     </div>
 </template>
 
 <script>
 import DmMessageThread from '../components/messageThread.vue';
 import ThreadCalendar from '../components/threadCalendar.vue';
+import InputField from '../components/InputField.vue';
 
 export default {
     name: 'DmPluginContents',
     components: {
         DmMessageThread,
         ThreadCalendar,
+        InputField,
     },
 };
 </script>
@@ -22,5 +25,8 @@ export default {
 .all-dm-messages {
     background: #fff;
     margin-bottom: 24px;
+    width: 100%;
+    min-height: 100%;
+    border-radius: 0.5rem;
 }
 </style>
